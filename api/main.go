99c3d0d5e53842fd7ad1dd32718883068bb0713d
lumino <luminovoez@gmail.com>
2024-07-29 18:47:27 +0800

    gin / middleware

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func Main() {
 	r := gin.Default()
 
+	r.Use(sessionMiddleware())
 	r.GET("/echo", func(c *gin.Context) {
 		c.String(200, "Hello, world!")
 	})
--- api/middleware.go
+++ api/middleware.go
@@ -1,18 +1,42 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Hana-ame/neo-moonchan/psql"
+	"github.com/gin-gonic/gin"
+)
 
 func registerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		country := c.GetHeader("Cf-Ipcountry")
 		ipAddress := c.GetHeader("Cf-Connecting-Ip")
-		session := c.GetHeader("M-Session")
+		// session := c.GetHeader("M-Session")
 		userAgent := c.GetHeader("User-Agent")
 
 		c.Set("country", country)
 		c.Set("ip", ipAddress)
-		c.Set("session", session)
+		// c.Set("session", session)
 		c.Set("ua", userAgent)
 	}
+}
 
+func sessionMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sessionID, err := c.Cookie("session_id")
+		if err != nil {
+			return
+		}
+
+		tx, err := psql.Begin()
+		if err != nil {
+			return
+		}
+		session, err := psql.GetSession(tx, sessionID)
+		if err != nil {
+			return
+		}
+		tx.Commit()
+
+		c.Set("session", sessionID)
+		c.Set("username", session.Username)
+	}
 }
